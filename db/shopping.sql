ed9ae5786fbfeeb17256abe32eb05cf6670bb285
thuynn1709 <31553009+thuynn1709@users.noreply.github.com>
2017-09-02 09:03:55 +0200

    2
    2

---
+++ db/shopping.sql
@@ -0,0 +1,279 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : ngocthuy
+Source Server Version : 50505
+Source Host           : localhost:3306
+Source Database       : shopping
+
+Target Server Type    : MYSQL
+Target Server Version : 50505
+File Encoding         : 65001
+
+Date: 2017-09-02 09:03:31
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for contact
+-- ----------------------------
+DROP TABLE IF EXISTS `contact`;
+CREATE TABLE `contact` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(40) DEFAULT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `content` varchar(200) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `status` tinyint(2) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of contact
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for export
+-- ----------------------------
+DROP TABLE IF EXISTS `export`;
+CREATE TABLE `export` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `author` varchar(30) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `type` tinyint(2) DEFAULT NULL COMMENT '0 = cho ai do, 1 = ban hang',
+  `amount` int(11) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `sale_off` int(11) DEFAULT NULL COMMENT 'phan tram giam gia',
+  `notice` varchar(100) DEFAULT NULL COMMENT 'ghi chú cần thiết',
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of export
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for extracost
+-- ----------------------------
+DROP TABLE IF EXISTS `extracost`;
+CREATE TABLE `extracost` (
+  `id` int(11) NOT NULL,
+  `name` varchar(500) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `author` varchar(50) DEFAULT NULL,
+  `notice` varchar(100) DEFAULT NULL,
+  `created` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of extracost
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for import
+-- ----------------------------
+DROP TABLE IF EXISTS `import`;
+CREATE TABLE `import` (
+  `id` int(11) NOT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `author` varchar(30) DEFAULT NULL,
+  `summer` int(11) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `versand` float DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of import
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for marken
+-- ----------------------------
+DROP TABLE IF EXISTS `marken`;
+CREATE TABLE `marken` (
+  `id` int(11) NOT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `country` varchar(30) DEFAULT NULL,
+  `addresse` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of marken
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for menu
+-- ----------------------------
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `name` varchar(40) DEFAULT NULL,
+  `level` tinyint(3) DEFAULT NULL,
+  `parent_id` tinyint(3) DEFAULT NULL,
+  `priority` tinyint(3) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of menu
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for news
+-- ----------------------------
+DROP TABLE IF EXISTS `news`;
+CREATE TABLE `news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `category_id` tinyint(2) DEFAULT NULL,
+  `desc` varchar(200) DEFAULT NULL,
+  `content` varchar(5000) DEFAULT NULL,
+  `author` varchar(40) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  `status` tinyint(2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of news
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for news_category
+-- ----------------------------
+DROP TABLE IF EXISTS `news_category`;
+CREATE TABLE `news_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `status` tinyint(2) DEFAULT NULL,
+  `menu_id` tinyint(3) DEFAULT NULL,
+  `priority` tinyint(2) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of news_category
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `status` tinyint(3) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of orders
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for products
+-- ----------------------------
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `category_id` tinyint(5) DEFAULT NULL,
+  `marken_id` tinyint(5) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `madein` varchar(10) DEFAULT NULL,
+  `img` varchar(100) DEFAULT NULL,
+  `img_thumb` varchar(100) DEFAULT NULL,
+  `img_1` varchar(100) DEFAULT NULL,
+  `img_2` varchar(100) DEFAULT NULL,
+  `img_3` varchar(100) DEFAULT NULL,
+  `describe` varchar(400) DEFAULT NULL,
+  `expired` datetime DEFAULT NULL,
+  `element` varchar(500) DEFAULT NULL COMMENT 'thanh phan san pham',
+  `warning` varchar(300) DEFAULT NULL,
+  `price` varchar(15) DEFAULT NULL,
+  `discount` varchar(10) DEFAULT NULL,
+  `status` tinyint(2) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of products
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for products_category
+-- ----------------------------
+DROP TABLE IF EXISTS `products_category`;
+CREATE TABLE `products_category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `status` tinyint(2) DEFAULT NULL,
+  `priority` tinyint(2) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of products_category
+-- ----------------------------
+INSERT INTO `products_category` VALUES ('1', 'fdafa', '1', '1', '2017-09-01 23:03:02');
+INSERT INTO `products_category` VALUES ('2', 'fafadf', '1', '3', '2017-09-01 23:03:02');
+INSERT INTO `products_category` VALUES ('3', 'fdaf', '0', '1', '2017-09-01 23:03:02');
+INSERT INTO `products_category` VALUES ('4', 'dfdafdaf', '0', '1', '2017-09-01 23:03:02');
+
+-- ----------------------------
+-- Table structure for sales
+-- ----------------------------
+DROP TABLE IF EXISTS `sales`;
+CREATE TABLE `sales` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `amount` int(11) DEFAULT NULL,
+  `discount` varchar(20) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of sales
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `fullname` varchar(100) DEFAULT NULL,
+  `address` varchar(300) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `group` varchar(5) NOT NULL,
+  `created` datetime DEFAULT NULL,
+  `updated` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
